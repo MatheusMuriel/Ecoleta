2eb14d25f6f44bb098fcd260a59e47ec2f4a155d
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-04 00:46:35 -0300

    Filter Points

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -3,6 +3,24 @@ import knex from '../database/connection';
 
 class PointsController {
 
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'point_id', '=', 'point_items.point_id')
+      .whereIn('point_items.point_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return response.json(points);
+  }
+
   async show (request: Request, response: Response) {
     const { id } = request.params;
 
@@ -14,7 +32,12 @@ class PointsController {
       return response.status(400).json({ message: 'Point not found.' });
     }
 
-    return response.json(point);
+    const items = await knex('items')
+      .join('point_items', 'item_id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+
+    return response.json({ point, items });
   }
 
   async create (request: Request, response: Response) {
