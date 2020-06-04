ef410a20e07c53eef2f54e425150b6fef8e394e9
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-04 02:24:39 -0300

    Conceitos React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+  }
+
   return (
-    <h1>Hellor World</h1>
+    <div>
+      <Header title="Hello World"></Header>
+
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    </div>
   );
 }
 
