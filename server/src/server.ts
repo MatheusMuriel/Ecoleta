acd1e4bb053e7c5c7fe8a76f0b6de024a7497d01
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-03 23:05:48 -0300

    Uploads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,12 @@
 import express, { response } from 'express';
+import path  from 'path';
 import routes from './routes';
 
 const app = express();
 
-app.use(express.json)
+app.use(express.json);
 app.use(routes)
 
-const users = [
-  'Diego',
-  'Cleiton',
-  'Robson',
-  'Daniel'
-];
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
