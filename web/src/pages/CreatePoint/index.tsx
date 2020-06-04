15e3e870f95c211a314ffb9029bcf3c8736b7644
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-04 14:08:52 -0300

    Select items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -43,14 +43,20 @@ interface IBGECidade {
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
   const [Ufs, setUfs] = useState<string[]>([]);
   const [Cities, setCities] = useState<string[]>([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
 
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
-  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -104,6 +110,23 @@ const CreatePoint = () => {
     ]);
   }
 
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([ ...selectedItems, id ]);
+    }
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -125,17 +148,17 @@ const CreatePoint = () => {
 
           <div className="field">
             <label htmlFor="name">Nome da entidade</label>
-            <input type="text" name="name" id="name"/>
+            <input type="text" name="name" id="name" onChange={handleInputChange} />
           </div>
 
           <div className="field-group">
             <div className="field">
               <label htmlFor="email">E-mail</label>
-              <input type="text" name="email" id="email"/>
+              <input type="text" name="email" id="email" onChange={handleInputChange} />
             </div>
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" name="whatsapp" id="whatsapp"/>
+              <input type="text" name="whatsapp" id="whatsapp" onChange={handleInputChange} />
             </div>
           </div>
 
@@ -187,7 +210,9 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map( item => (
-              <li key={item.id}>
+              <li key={item.id} onClick={() => handleSelectItem(item.id)} 
+                className={selectedItems.includes(item.id) ? 'selected' : ''} 
+              >
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
