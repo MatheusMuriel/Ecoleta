60498d285aea90df72f8142c033c02f6170d8b93
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-04 15:47:09 -0300

    Updates

---
+++ precache-manifest.22fcbcfd9af01c6cfb03b83b871e466e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d69de4f96f9e9c05080d775ff51046",
+    "url": "/Ecoleta/index.html"
+  },
+  {
+    "revision": "c443f51c08d1e8a9b88a",
+    "url": "/Ecoleta/static/css/main.72d166d0.chunk.css"
+  },
+  {
+    "revision": "f65ea9b56277bd0a76b0",
+    "url": "/Ecoleta/static/js/2.a8acb5af.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/Ecoleta/static/js/2.a8acb5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c443f51c08d1e8a9b88a",
+    "url": "/Ecoleta/static/js/main.f2f7dcdc.chunk.js"
+  },
+  {
+    "revision": "125feb4fbd24e92e75c3",
+    "url": "/Ecoleta/static/js/runtime-main.0371082f.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/Ecoleta/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/Ecoleta/static/media/logo.47c4581a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ef69a4b448066ad30fdc0185311f468.js"
+  "/Ecoleta/precache-manifest.22fcbcfd9af01c6cfb03b83b871e466e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ecoleta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
