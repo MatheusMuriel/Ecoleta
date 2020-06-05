0eb2af99c16e5a7dabffa7ae96ecf14af0d12311
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-05 10:54:02 -0300

    Working without routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,6 +6,7 @@ import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
 import Routes from './src/routes';
+import Home from './src/pages/Home';
 
 export default function App() {
   const [fontsLoades] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
-      <Routes />
+      <Home />
     </>
   );
 }
