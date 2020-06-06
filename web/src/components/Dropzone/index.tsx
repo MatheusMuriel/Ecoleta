fb12d66b87fc39eea3cb951886eb9f5dc98068e5
Matheus Muriel <matheus.muriel@outlook.com>
2020-06-06 16:29:51 -0300

    Comunicação entre os componentes

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -4,7 +4,11 @@ import { FiUpload } from "react-icons/fi";
 
 import "./styles.css";
 
-const Dropzone = () => {
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
   const [selectedFileUrl, setSelectedFileUrl] = useState('');
 
   const onDrop = useCallback(acceptedFiles => {
@@ -13,6 +17,7 @@ const Dropzone = () => {
     const fileUrl = URL.createObjectURL(file);
 
     setSelectedFileUrl(fileUrl);
+    onFileUploaded(file);
   }, [])
 
   const {getRootProps, getInputProps} = useDropzone({
